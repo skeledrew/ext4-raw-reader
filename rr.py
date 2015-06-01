6e99d3fb03420f79548ddfdf767050de4e3fda6d
Andrew Phillips <skeledrew@gmail.com>
2015-06-01 08:24:05 -0500

    Added AGPL notice.

--- rr.py
+++ rr.py
@@ -1,5 +1,21 @@
 #! /bin/python
 
+#  EXT4 Raw Reader: Read the raw contents of EXT4 partitions or their images for recovery, etc.
+#  Copyright (C) 2015 Andrew Phillips <skeledrew@gmail.com>
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Affero General Public License as published
+#  by the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Affero General Public License for more details.
+#
+#  You should have received a copy of the GNU Affero General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 """Docstring template.
   
   Created:
@@ -26,7 +42,7 @@ import time
 """Raw Reader.
   
   Created:
-   15-05-23
+   14-07-??
   Last Modified:
    n/a
    
